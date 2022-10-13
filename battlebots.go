d94e816e5ebd8a28e9b1bf55cd95e03699f8922a
fergcb <fergus.bentley@gmail.com>
2022-10-13 10:59:04 +0100

    Refactor control program

--- battlebots.go
+++ battlebots.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 )
 
-const NUMBOTS = 2
 const BOUTSPERMATCH = 5
 const ROUNDSPERBOUT = 1000
 const MACFILENAMESIZE = 100
@@ -16,27 +15,77 @@ const DISPLAYBOUTS = false
 const WIDTH = 10
 const HEIGHT = 10
 
-var directions = []string{"N", "NE", "E", "SE", "S", "SW", "W", "NW"}
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func max(a int, b int) int {
+	if a < b {
+		return b
+	}
+
+	return a
+}
+
+type Vector struct {
+	x int
+	y int
+}
+
+var directions = map[string]Vector{
+	"N":  {0, -1},
+	"NE": {1, -1},
+	"E":  {1, 0},
+	"SE": {1, 1},
+	"S":  {0, 1},
+	"SW": {-1, 1},
+	"W":  {-1, 0},
+	"NW": {-1, -1},
+}
+
+func (v1 Vector) equals(v2 Vector) bool {
+	return v1.x == v2.x && v1.y == v2.y
+}
+
+func (v1 Vector) dist(v2 Vector) int {
+	return max(abs(v1.x-v2.x), abs(v1.y-v2.y))
+}
+
+func (v1 Vector) isAdjacentTo(v2 Vector) bool {
+	return v1.dist(v2) < 2
+}
+
+func (v1 Vector) add(v2 Vector) Vector {
+	return Vector{v1.x + v2.x, v1.y + v2.y}
+}
+
+func (v Vector) inBounds() bool {
+	return v.x > 0 && v.y > 0 && v.x < WIDTH && v.y < HEIGHT
+}
 
 type Bot struct {
 	name   string
 	cmd    string
-	x      int
-	y      int
+	pos    Vector
 	hp     int
 	action string
 }
 
-type Landmine struct {
-	x    int
-	y    int
-	dead bool
+func newBot(name string, cmd string) *Bot {
+	return &Bot{name, cmd, Vector{}, 10, "NO"}
 }
 
+type Landmine struct {
+	pos  Vector
+	dead bool
+}
 type Projectile struct {
-	x    int
-	y    int
-	dir  int
+	pos  Vector
+	dir  string
 	dead bool
 }
 
@@ -46,35 +95,50 @@ type Weapons struct {
 	landmines []*Landmine
 }
 
+func newWeapons() *Weapons {
+	return &Weapons{
+		make([]*Projectile, 0),
+		make([]*Projectile, 0),
+		make([]*Landmine, 0),
+	}
+}
+
 func main() {
-	bots := [NUMBOTS]*Bot{
+	bots := []*Bot{
 		newBot("huggy", "python ./bots/huggy.py"),
 		newBot("nop", "java bots.Nop"),
 	}
+
+	numBots := len(bots)
 
 	botDir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	boutsWon := make([]int, NUMBOTS)
-	matchesWon := make([]int, NUMBOTS)
+	boutsWon := make([]int, numBots)
+	matchesWon := make([]int, numBots)
 
-	for i := 0; i < NUMBOTS; i++ {
+	for i := 0; i < numBots; i++ {
 		boutsWon[i] = 0
 		matchesWon[i] = 0
 	}
 
-	for b1 := 0; b1 < NUMBOTS-1; b1++ {
-		for b2 := b1 + 1; b2 < NUMBOTS; b2++ {
+	for b1 := 0; b1 < numBots-1; b1++ {
+		for b2 := b1 + 1; b2 < numBots; b2++ {
 			bot1, bot2 := bots[b1], bots[b2]
 			bot1Bouts, bot2Bouts := 0, 0
+
 			fmt.Printf("%s vs %s\n", bots[b1].name, bots[b2].name)
+
 			for bout := 0; bout < BOUTSPERMATCH; bout++ {
 				fmt.Printf("%d ", bout)
-				bot1.x, bot1.y, bot1.hp = 1, 1, 10
-				bot2.x, bot2.y, bot2.hp = WIDTH-2, HEIGHT-2, 10
-				weapons := initWeapons()
+
+				bot1.pos, bot1.hp = Vector{1, 1}, 10
+				bot2.pos, bot2.hp = Vector{WIDTH - 2, HEIGHT - 2}, 10
+
+				weapons := newWeapons()
+
 				paralyzedRoundsRemaining := 0
 
 				for round := 0; round < ROUNDSPERBOUT; round++ {
@@ -90,65 +154,8 @@ func main() {
 					}
 
 					if paralyzedRoundsRemaining == 0 {
-						// Move bot 1
-						bot1Moved := false
-						dx, dy := 0, 0
-						dx = getXMove(bot1.action)
-						dy = getYMove(bot1.action)
-						if newPosInBounds(bot1.x, bot1.y, dx, dy) {
-							if !(bot1.x+dx == bot2.x) || !(bot1.y+dy == bot2.y) {
-								bot1Moved = true
-								bot1.x += dx
-								bot1.y += dy
-							}
-						}
-
-						// Move bot 2
-						dx = getXMove(bot2.action)
-						dy = getYMove(bot2.action)
-						if newPosInBounds(bot2.x, bot2.y, dx, dy) {
-							if !(bot2.x+dx == bot1.x) || !(bot2.y+dy == bot1.y) {
-								bot2.x += dx
-								bot2.y += dy
-							}
-						}
-
-						// If bot2 was in the way the first time, let bot 1 try again
-						if !bot1Moved {
-							dx = getXMove(bot1.action)
-							dy = getYMove(bot1.action)
-							if newPosInBounds(bot1.x, bot1.y, dx, dy) {
-								if !(bot1.x+dx == bot2.x) || !(bot1.y+dy == bot2.y) {
-									bot1.x += dx
-									bot1.y += dy
-								}
-							}
-						}
-
-						for _, l := range weapons.landmines {
-							if directHit(bot1, l.x, l.y) {
-								bot1.hp -= 2
-								if inShrapnelRange(bot2, l.x, l.y) {
-									bot2.hp -= 1
-								}
-								l.dead = true
-							} else if directHit(bot2, l.x, l.y) {
-								bot2.hp -= 2
-								if inShrapnelRange(bot1, l.x, l.y) {
-									bot1.hp -= 1
-								}
-								l.dead = true
-							}
-						}
-
-						n := 0
-						for _, l := range weapons.landmines {
-							if !l.dead {
-								weapons.landmines[n] = l
-								n += 1
-							}
-						}
-						weapons.landmines = weapons.landmines[:n]
+						moveBots(bot1, bot2)
+						checkLandmines(bot1, bot2, weapons)
 					} else {
 						paralyzedRoundsRemaining -= 1
 						if DISPLAYBOUTS {
@@ -167,87 +174,8 @@ func main() {
 					deployWeapons(bot1, bot2, weapons)
 					deployWeapons(bot2, bot1, weapons)
 
-					for _, b := range weapons.bullets {
-						dx := getXMove(directions[b.dir])
-						dy := getYMove(directions[b.dir])
-
-						for moves := 0; moves < 3; moves++ {
-							if newPosInBounds(b.x, b.y, dx, dy) {
-								b.x += dx
-								b.y += dy
-
-								if directHit(bot1, b.x, b.y) {
-									bot1.hp -= 1
-									b.dead = true
-									break
-								}
-
-								if directHit(bot2, b.x, b.y) {
-									bot2.hp -= 1
-									b.dead = true
-									break
-								}
-							} else {
-								b.dead = true
-							}
-						}
-					}
-
-					n := 0
-					for _, b := range weapons.bullets {
-						if !b.dead {
-							weapons.bullets[n] = b
-							n += 1
-						}
-					}
-					weapons.bullets = weapons.bullets[:n]
-
-					for _, m := range weapons.missiles {
-						dx := getXMove(directions[m.dir])
-						dy := getYMove(directions[m.dir])
-
-						for moves := 0; moves < 2; moves++ {
-							if newPosInBounds(m.x, m.y, dx, dy) {
-								m.x += dx
-								m.y += dy
-
-								if directHit(bot1, m.x, m.y) {
-									bot1.hp -= 3
-									if inShrapnelRange(bot2, m.x, m.y) {
-										bot2.hp -= 1
-									}
-									m.dead = true
-									break
-								}
-
-								if directHit(bot2, m.x, m.y) {
-									bot2.hp -= 3
-									if inShrapnelRange(bot1, m.x, m.y) {
-										bot1.hp -= 1
-									}
-									m.dead = true
-									break
-								}
-							} else {
-								if inShrapnelRange(bot1, m.x, m.y) {
-									bot1.hp -= 1
-								}
-								if inShrapnelRange(bot2, m.x, m.y) {
-									bot2.hp -= 1
-								}
-								m.dead = true
-							}
-						}
-					}
-
-					n = 0
-					for _, m := range weapons.missiles {
-						if !m.dead {
-							weapons.missiles[n] = m
-							n += 1
-						}
-					}
-					weapons.missiles = weapons.missiles[:n]
+					moveBullets(bot1, bot2, weapons)
+					moveMissiles(bot1, bot2, weapons)
 
 					if bot1.hp < 1 || bot2.hp < 1 {
 						break
@@ -275,20 +203,8 @@ func main() {
 
 	fmt.Println("\nResults:")
 	fmt.Println("Bot\tMatches\tBouts")
-	for i := 0; i < NUMBOTS; i++ {
+	for i := 0; i < numBots; i++ {
 		fmt.Printf("%s\t%d\t%d\n", bots[i].name, matchesWon[i], boutsWon[i])
-	}
-}
-
-func newBot(name string, cmd string) *Bot {
-	return &Bot{name, cmd, 0, 0, 10, "NO"}
-}
-
-func initWeapons() *Weapons {
-	return &Weapons{
-		make([]*Projectile, 0),
-		make([]*Projectile, 0),
-		make([]*Landmine, 0),
 	}
 }
 
@@ -313,18 +229,18 @@ func drawArena(bot *Bot, enemy *Bot, weapons *Weapons) string {
 	clearArena(grid)
 	for i := range weapons.bullets {
 		b := weapons.bullets[i]
-		grid[b.y][b.x] = 'B'
-		stats.WriteString(fmt.Sprintf("B x=%d y=%d dir=%s\n", b.x, b.y, directions[b.dir]))
+		grid[b.pos.y][b.pos.x] = 'B'
+		stats.WriteString(fmt.Sprintf("B x=%d y=%d dir=%s\n", b.pos.x, b.pos.y, b.dir))
 	}
 
 	for i := range weapons.missiles {
 		m := weapons.missiles[i]
-		grid[m.y][m.x] = 'M'
-		stats.WriteString(fmt.Sprintf("M x=%d y=%d dir=%s\n", m.x, m.y, directions[m.dir]))
+		grid[m.pos.y][m.pos.x] = 'M'
+		stats.WriteString(fmt.Sprintf("M x=%d y=%d dir=%s\n", m.pos.x, m.pos.y, m.dir))
 	}
 
-	grid[bot.y][bot.x] = 'Y'
-	grid[enemy.y][enemy.x] = 'X'
+	grid[bot.pos.y][bot.pos.x] = 'Y'
+	grid[enemy.pos.y][enemy.pos.x] = 'X'
 	// fmt.Println(bot.x, bot.y)
 	// fmt.Println(enemy.x, enemy.y)
 
@@ -352,105 +268,95 @@ func runBot(bot *Bot, arena string, dir string) string {
 	return strings.TrimSpace(string(stdout))
 }
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
+func moveBots(bot1 *Bot, bot2 *Bot) {
+	// Move bot 1
+	bot1Moved := false
+	v := directions[bot1.action]
+	newPos := bot1.pos.add(v)
+	if newPos.inBounds() && !newPos.equals(bot2.pos) {
+		bot1Moved = true
+		bot1.pos = newPos
 	}
 
-	return x
-}
-
-func getXMove(dir string) int {
-	if dir == "NE" {
-		return 1
-	} else if dir == "E" {
-		return 1
-	} else if dir == "SE" {
-		return 1
-	} else if dir == "SW" {
-		return -1
-	} else if dir == "W" {
-		return -1
-	} else if dir == "NW" {
-		return -1
+	// Move bot 2
+	v = directions[bot2.action]
+	newPos = bot2.pos.add(v)
+	if newPos.inBounds() && !newPos.equals(bot1.pos) {
+		bot2.pos = newPos
 	}
-	return 0
-}
 
-func getYMove(dir string) int {
-	if dir == "S" {
-		return 1
-	} else if dir == "SE" {
-		return 1
-	} else if dir == "SW" {
-		return 1
-	} else if dir == "N" {
-		return -1
-	} else if dir == "NE" {
-		return -1
-	} else if dir == "NW" {
-		return -1
+	// If bot2 was in the way the first time, let bot 1 try again
+	if !bot1Moved {
+		v := directions[bot1.action]
+		newPos := bot1.pos.add(v)
+		if newPos.inBounds() && !newPos.equals(bot2.pos) {
+			bot1.pos = newPos
+		}
 	}
-	return 0
 }
 
-func newPosInBounds(ox int, oy int, dx int, dy int) bool {
-	return ox+dx >= 0 && ox+dx < WIDTH && oy+dy >= 0 && oy+dy < HEIGHT
-}
-
-func directHit(b *Bot, x int, y int) bool {
-	return b.x == x && b.y == y
-}
-
-func landmineCollision(l1 *Landmine, l2 *Landmine) bool {
-	return l1.x == l2.x && l1.y == l2.y
-}
-
-func inShrapnelRange(b *Bot, x int, y int) bool {
-	return abs(b.x-x) < 2 && abs(b.y-y) < 2
-}
-
-func directionToInt(dir string) int {
-	for i := range directions {
-		if dir == directions[i] {
-			return i
+func checkLandmines(bot1 *Bot, bot2 *Bot, weapons *Weapons) {
+	for _, l := range weapons.landmines {
+		if bot1.pos.equals(l.pos) {
+			bot1.hp -= 2
+			if bot2.pos.isAdjacentTo(l.pos) {
+				bot2.hp -= 1
+			}
+			l.dead = true
+		} else if bot2.pos.equals(l.pos) {
+			bot2.hp -= 2
+			if bot1.pos.isAdjacentTo(l.pos) {
+				bot1.hp -= 1
+			}
+			l.dead = true
 		}
 	}
 
-	return -1
-}
-
-func newProjectile(x int, y int, dir string) *Projectile {
-	return &Projectile{x, y, directionToInt(dir), false}
+	n := 0
+	for _, l := range weapons.landmines {
+		if !l.dead {
+			weapons.landmines[n] = l
+			n += 1
+		}
+	}
+	weapons.landmines = weapons.landmines[:n]
 }
 
 func deployWeapons(bot *Bot, enemy *Bot, weapons *Weapons) {
 	fields := strings.Fields(bot.action)
+
+	if len(fields) != 2 {
+		return
+	}
+
+	if _, ok := directions[fields[1]]; !ok {
+		return
+	}
+
 	weapon := fields[0]
+	dir := fields[1]
+
 	if weapon == "B" {
-		bullet := newProjectile(bot.x, bot.y, fields[1])
-		if bullet.dir != -1 {
-			weapons.bullets = append(weapons.bullets, bullet)
-		}
+		bullet := &Projectile{bot.pos, dir, false}
+		weapons.bullets = append(weapons.bullets, bullet)
 	} else if weapon == "M" {
-		missile := newProjectile(bot.x, bot.y, fields[1])
-		if missile.dir != 1 {
-			weapons.missiles = append(weapons.missiles, missile)
-		}
+		missile := &Projectile{bot.pos, dir, false}
+		weapons.missiles = append(weapons.missiles, missile)
 	} else if weapon == "L" {
-		dir := fields[1]
-		if newPosInBounds(bot.x, bot.y, getXMove(dir), getYMove(dir)) {
+		targetPos := bot.pos.add(directions[dir])
+		if targetPos.inBounds() {
 			var landmine *Landmine
-			landmine.x = bot.x + getXMove(dir)
-			landmine.y = bot.y + getYMove(dir)
+
+			landmine.pos = targetPos
+
 			var collision bool
 			for i := range weapons.landmines {
 				l := weapons.landmines[i]
-				if landmineCollision(landmine, l) {
-					if inShrapnelRange(bot, l.x, l.y) {
+				if landmine.pos.equals(l.pos) {
+					if bot.pos.isAdjacentTo(l.pos) {
 						bot.hp -= 1
 					}
-					if inShrapnelRange(enemy, l.x, l.y) {
+					if enemy.pos.isAdjacentTo(l.pos) {
 						enemy.hp -= 1
 					}
 
@@ -465,6 +371,90 @@ func deployWeapons(bot *Bot, enemy *Bot, weapons *Weapons) {
 			}
 		}
 	}
+}
+
+func moveBullets(bot1 *Bot, bot2 *Bot, weapons *Weapons) {
+	for _, b := range weapons.bullets {
+		v := directions[b.dir]
+		for moves := 0; moves < 3; moves++ {
+			newPos := b.pos.add(v)
+			if newPos.inBounds() {
+				b.pos = newPos
+
+				if bot1.pos.equals(b.pos) {
+					bot1.hp -= 1
+					b.dead = true
+					break
+				}
+
+				if bot2.pos.equals(b.pos) {
+					bot2.hp -= 1
+					b.dead = true
+					break
+				}
+			} else {
+				b.dead = true
+			}
+		}
+	}
+
+	n := 0
+	for _, b := range weapons.bullets {
+		if !b.dead {
+			weapons.bullets[n] = b
+			n += 1
+		}
+	}
+	weapons.bullets = weapons.bullets[:n]
+}
+
+func moveMissiles(bot1 *Bot, bot2 *Bot, weapons *Weapons) {
+	for _, m := range weapons.missiles {
+		v := directions[m.dir]
+
+		for moves := 0; moves < 2; moves++ {
+			newPos := m.pos.add(v)
+			if newPos.inBounds() {
+				m.pos = newPos
+
+				if bot1.pos.equals(m.pos) {
+					bot1.hp -= 3
+					if bot2.pos.isAdjacentTo(m.pos) {
+						bot2.hp -= 1
+					}
+					m.dead = true
+					break
+				}
+
+				if bot2.pos.equals(m.pos) {
+					bot2.hp -= 3
+					if bot1.pos.isAdjacentTo(m.pos) {
+						bot1.hp -= 1
+					}
+					m.dead = true
+					break
+				}
+			} else {
+				if bot1.pos.isAdjacentTo(m.pos) {
+					bot1.hp -= 1
+				}
+				if bot2.pos.isAdjacentTo(m.pos) {
+					bot2.hp -= 1
+				}
+				m.dead = true
+			}
+		}
+	}
+
+	n := 0
+	for _, m := range weapons.missiles {
+		if !m.dead {
+			weapons.missiles[n] = m
+			n += 1
+		}
+	}
+	weapons.missiles = weapons.missiles[:n]
+
 }
 
 func clearArena(arena [][]rune) {
